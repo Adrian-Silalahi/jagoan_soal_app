91749ef05c034854fa1c8af91795ba8e8065d295
Adrianus Silalahi <adrianussilalahi766@gmail.com>
2023-12-03 11:08:30 +0700

    menghapus libs yang tidak perlu

--- app/api/asal-post/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export default async function POST(req: Request) {
-    const request = await req.json()
-    console.log('woiiiiiiiiiiiiiiiiiii');
-    
-    return NextResponse.json({ data: 'Hello, Next.js!', status: 200 });
-}
