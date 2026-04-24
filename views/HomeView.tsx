d385f3f64890f35c8982ca1d2ac83d2c32fe7cf8
Adrianus Silalahi <211112269@students.mikroskil.ac.id>
2026-04-24 14:27:26 +0700

    delete tech stack side

--- views/HomeView.tsx
+++ views/HomeView.tsx
@@ -1,22 +1,19 @@
 "use client"
-import { SessionProvider } from 'next-auth/react'
-import React from 'react'
 
-import SectionFeature from '../components/home/SectionFeature'
-import SectionHeader from '../components/home/SectionHeader'
-import SectionTechStack from '../components/home/SectionTechStack'
-import SectionVideo from '../components/home/SectionVideo'
+import React from "react"
+import { SessionProvider } from "next-auth/react"
+
+import SectionFeature from "../components/home/SectionFeature"
+import SectionHeader from "../components/home/SectionHeader"
+import SectionVideo from "../components/home/SectionVideo"
 
 const HomeView = () => {
-
   return (
     <SessionProvider>
-      <div className='-mt-24 bg-[#0A0A0F] text-on-background min-h-screen overflow-x-hidden selection:bg-primary-container selection:text-on-primary-container'>
+      <div className="-mt-24 bg-[#0A0A0F] text-on-background min-h-screen overflow-x-hidden selection:bg-primary-container selection:text-on-primary-container">
         <SectionHeader />
         <SectionVideo />
         <SectionFeature />
-        <SectionTechStack />
-
       </div>
     </SessionProvider>
   )
